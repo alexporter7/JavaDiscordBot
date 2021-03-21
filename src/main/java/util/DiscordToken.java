016748cca7ca2bc765c2f4d300e1093015a5daf7
Alex <Alex.Porter@ucf.edu>
2021-03-21 11:41:11 -0400

    Initial commit

---
+++ src/main/java/util/DiscordToken.java
@@ -0,0 +1,15 @@
+package util;
+
+public class DiscordToken {
+
+    public final String discordToken;
+
+    public DiscordToken(String token) {
+        this.discordToken = token;
+    }
+
+    public String getToken() {
+        return this.discordToken;
+    }
+
+}
