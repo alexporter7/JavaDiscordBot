72f64e441f37896c969fd5861a8db1550841491e
Alex <Alex.Porter@ucf.edu>
2021-04-03 16:07:03 -0400

    Added logic to now have bot build embedded messages and reply in a specific channel defined in the config (src/main/util/Config)

--- src/main/java/commands/GetWeatherCommand.java
+++ src/main/java/commands/GetWeatherCommand.java
@@ -1,5 +1,6 @@
 package commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import util.Config;
 import com.jagrosh.jdautilities.command.Command;
@@ -11,6 +12,7 @@ import okhttp3.Response;
 import util.WeatherDataParser;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GetWeatherCommand extends Command {
 
@@ -31,16 +33,15 @@ public class GetWeatherCommand extends Command {
         try {
             Response response = httpClient.newCall(request).execute();
             WeatherDataParser weatherDataParser = new WeatherDataParser(response);
-
-            commandEvent.reply(String.format(
-                    "Current weather is `%s`%n" +
-                            "Temperature outside is `%s`%n" +
-                            "Humidity right now is `%s`",
-                    weatherDataParser.getWeatherDescription(),
-                    weatherDataParser.getCurrentTemperature(),
-                    weatherDataParser.getCurrentHumidity()
-            ));
-
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder
+                    .setTitle("Weather at UCF")
+                    .addField("Current Weather: ", weatherDataParser.getWeatherDescription(), true)
+                    .addField("Temperature Outside: ", weatherDataParser.getCurrentTemperature(), true)
+                    .addField("Humidity(%): ", weatherDataParser.getCurrentHumidity(), true);
+            Objects.requireNonNull(
+                    commandEvent.getJDA().getTextChannelById(Config.WEATHER_CHANNEL))
+                                .sendMessage(embedBuilder.build()).queue();
         } catch (IOException e) {
             e.printStackTrace();
             commandEvent.reply("Unable to grab current weather data");
